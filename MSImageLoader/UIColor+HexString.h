fed2acc432fa25bba159db986681f4cc48c1ac46
Mohammad Shaker <mohammad.shaker.103@gmail.com>
2017-07-30 00:10:14 +0200

    Add category for using hex colors

---
+++ MSImageLoader/UIColor+HexString.h
@@ -0,0 +1,15 @@
+//
+//  UIColor+HexString.h
+//  MSImageLoader
+//
+//  Created by Mohammad Shaker on 7/29/17.
+//  Copyright © 2017 Mohammad Shaker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (HexString)
+
++ (UIColor *)colorFromHexString:(NSString *)hexString;
+
+@end
---
+++ MSImageLoader/UIColor+HexString.m
@@ -0,0 +1,23 @@
+//
+//  UIColor+HexString.m
+//  MSImageLoader
+//
+//  Created by Mohammad Shaker on 7/29/17.
+//  Copyright © 2017 Mohammad Shaker. All rights reserved.
+//
+
+#import "UIColor+HexString.h"
+
+@implementation UIColor (HexString)
+
+
++ (UIColor *)colorFromHexString:(NSString *)hexString {
+    unsigned rgbValue = 0;
+    NSScanner *scanner = [NSScanner scannerWithString:hexString];
+    [scanner setScanLocation:1]; // bypass '#' character
+    [scanner scanHexInt:&rgbValue];
+    return [UIColor colorWithRed:((rgbValue & 0xFF0000) >> 16)/255.0 green:((rgbValue & 0xFF00) >> 8)/255.0 blue:(rgbValue & 0xFF)/255.0 alpha:1.0];
+}
+
+
+@end
